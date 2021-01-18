f9e10479300899196bbe2f1f68fc687f5918ceae
senghe <senghe@gmail.com>
2021-01-18 09:16:43 +0100

    Unify Open-Source header comment-block

--- src/Repository/ByrdProductMappingRepositoryInterface.php
+++ src/Repository/ByrdProductMappingRepositoryInterface.php
@@ -4,7 +4,7 @@
  * This file has been created by developers from BitBag.
  * Feel free to contact us once you face any issues or want to start
  * another great project.
- * You can find more information about us on https://bitbag.io and write us
+ * You can find more information about us on https://bitbag.shop and write us
  * an email on mikolaj.krol@bitbag.pl.
  */
 
